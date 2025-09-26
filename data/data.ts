9e30c95e25529acb593b84ec88837612d5164b02
visbran <brandonvisca@gmail.com>
2025-09-26 22:19:22 +0200

    fix

--- data/data.ts
+++ data/data.ts
@@ -1,5 +1,32 @@
+type SocialIcon = {
+    icon: string;
+    link: string;
+};
+
+type HideConfig = {
+    subscribeForm: boolean;
+    header: boolean;
+    content: boolean;
+    footer: boolean;
+};
+
+type SiteData = {
+    sitename: string;
+    sitetagline: string;
+    siteurl: string;
+    sitelogo: string;
+    title: string;
+    description: string;
+    newsletterheading: string;
+    copyrightText: string;
+    socialIconsHeading: string;
+    hideSubscribeForm: boolean;
+    socialIcons: SocialIcon[];
+    hide: HideConfig;
+};
+
 const currentYear = new Date().getFullYear();
-const data = {
+const data: SiteData = {
     sitename: "LMF Solutions",
     sitetagline: "Quelque chose de grand se prépare ! ✨ 🔥",
     siteurl: "https://lmfsolutions.fr",
@@ -20,3 +47,4 @@ const data = {
 }
 
 export default data;
+export type { SiteData, SocialIcon };
