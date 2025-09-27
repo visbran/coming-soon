e673e399b3defc34d41a0482684412811b76e1de
visbran <brandonvisca@gmail.com>
2025-09-27 13:30:47 +0200

    one page comming soon

--- data/data.ts
+++ data/data.ts
@@ -3,13 +3,6 @@ type SocialIcon = {
     link: string;
 };
 
-type HideConfig = {
-    subscribeForm: boolean;
-    header: boolean;
-    content: boolean;
-    footer: boolean;
-};
-
 type SiteData = {
     sitename: string;
     sitetagline: string;
@@ -17,12 +10,9 @@ type SiteData = {
     sitelogo: string;
     title: string;
     description: string;
-    newsletterheading: string;
     copyrightText: string;
     socialIconsHeading: string;
-    hideSubscribeForm: boolean;
     socialIcons: SocialIcon[];
-    hide: HideConfig;
 };
 
 const currentYear = new Date().getFullYear();
@@ -33,18 +23,10 @@ const data: SiteData = {
     sitelogo: "",
     title: "Bientôt disponible !",
     description: "Nous travaillons actuellement 👨‍💻 d'arrache-pied pour vous offrir quelque chose d'exceptionnel, et nous sommes impatients de le partager avec vous 📅. Notre équipe met la touche finale à un nouveau projet 🚀 que vous allez adorer 😍.",
-    newsletterheading: "Restez informés de nos actualités !",
     copyrightText: `Copyright © ${currentYear} | LMF Solutions`,
     socialIconsHeading: "Suivez-nous 📣",
-    hideSubscribeForm: false, // make true to disable subscription form 
     socialIcons: [],
-    hide :{
-        subscribeForm: false, // make true to disable subscription form         
-        header: false,
-        content: false,
-        footer: false,
-    }
-}
+};
 
 export default data;
 export type { SiteData, SocialIcon };
--- src/components/StandardComingSoon/StandardComingSoon.tsx
+++ src/components/StandardComingSoon/StandardComingSoon.tsx
@@ -1,4 +1,3 @@
-import SubscribeForm from "../SubscribeForm/SubscribeForm";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import ComingSoonContent from "../ComingSoonContent/ComingSoonContent";
@@ -8,7 +7,6 @@ export default function StandardComingSoon() {
     <main className="flex min-h-screen flex-col items-center justify-between p-5 lg:p-12">
       <Header />
       <ComingSoonContent />
-      <SubscribeForm />
       <Footer />
     </main>
   );
